fb6d4f656b4cd04623dac6ab045fca63d52322b9
MD Ashiqur Rahman Joy <43420293+XLR8-07@users.noreply.github.com>
2022-12-27 02:02:24 -0800

    firebase implementation

--- App.js
+++ App.js
@@ -5,8 +5,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import SignInScreen from "./src/screens/SignInScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
+import * as firebase from "firebase/app";
 
 import { AuthProvider, AuthContext } from "./src/providers/AuthProvider";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoeN-L5rC-G5-o3w29C3EmMhh72JiMVs",
+  authDomain: "mad-fall22.firebaseapp.com",
+  projectId: "mad-fall22",
+  storageBucket: "mad-fall22.appspot.com",
+  messagingSenderId: "939369558974",
+  appId: "1:939369558974:web:962128e2a0909b64b8ffef",
+};
+if (!firebase.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const HomeStack = createStackNavigator();
 const AuthStack = createStackNavigator();
@@ -21,7 +34,7 @@ const HomeStackScreen = () => {
 
 const AuthStackScreen = () => {
   return (
-    <AuthStack.Navigator initialRouteName="SignIn">
+    <AuthStack.Navigator initialRouteName="SignUp">
       <AuthStack.Screen name="SignIn" component={SignInScreen} />
       <AuthStack.Screen name="SignUp" component={SignUpScreen} />
     </AuthStack.Navigator>
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,16 +1,91 @@
+import { Input, Button, Card } from "@rneui/themed";
 import React, { useState } from "react";
-import { View, Button, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as firebase from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import { getDatabase } from "firebase/database";
 
 const SignUpScreen = (props) => {
+  const [name, setName] = useState("");
+  const [studentID, setStudentID] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUp = () => {
+    const auth = getAuth();
+    if (name && email && studentID && password) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCreds) => {
+          // userCreds.user.updateProfile({ displayName: name });
+          // const database = getDatabase();
+          // const ref = database.ref("https://mad-fall22-default-rtdb.firebaseio.com");
+          // ref
+          //   .child("users/")
+          //   .child(userCreds.user.uid)
+          //   .set({
+          //     name: name,
+          //     sid: studentID,
+          //     email: email,
+          //   })
+          //   .then(() => {
+          //     alert("account created successfully");
+          //     props.navigation.navigate("SignIn");
+          //   });
+          console.log(userCreds);
+        })
+        .catch((error) => {
+          console.log("Firebase Error", error);
+        });
+    } else {
+      alert("FIELDS ARE EMPTY!");
+    }
+  };
+
   return (
-    <View>
-      <Text style={styles.textStyle}>Welcome To SignUpScreen</Text>
-      <Button
-        title="Already Have an Account? Sign In!"
-        onPress={() => {
-          props.navigation.navigate("SignIn");
-        }}
-      />
+    <View style={styles.viewStyle}>
+      <Card>
+        <Card.Title>Welcome to Auth App</Card.Title>
+        <Card.Divider />
+        <Input
+          placeholder="Name"
+          onChangeText={(currentInput) => {
+            setName(currentInput);
+          }}
+        />
+        <Input
+          placeholder="Student ID"
+          onChangeText={(currentInput) => {
+            setStudentID(currentInput);
+          }}
+        />
+        <Input
+          placeholder="Email"
+          onChangeText={(currentInput) => {
+            setEmail(currentInput);
+          }}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={(currentInput) => {
+            setPassword(currentInput);
+          }}
+        />
+        <Button
+          title="Sign Up!"
+          type="solid"
+          onPress={() => {
+            handleSignUp();
+          }}
+        />
+        <Button
+          title="Already Have an Account?"
+          type="clear"
+          onPress={() => {
+            props.navigation.navigate("SignIn");
+          }}
+        />
+      </Card>
     </View>
   );
 };
@@ -19,6 +94,11 @@ const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
     color: "blue",
+  },
+  viewStyle: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#4bacb8",
   },
 });
 
